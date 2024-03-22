cf05e9d700066204f3d40beea334d6d749f9508f
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-03-22 14:14:13 +0530

    code format

---
+++ app/api/reconstructkey/route.js
@@ -0,0 +1,23 @@
+// route.js
+import { NextResponse } from "next/server";
+import { generateSecret } from "../../utils/reconstruct.js";
+
+export const POST = async (req, res) => {
+  try {
+    const body = await req.json();
+
+    const { shares, threshold } = body;
+    const keys = body.keys;
+
+    const recoveredSecrets = [];
+
+    for (let i = shares; i >= 1; i--) {
+      const recoveredSecret = generateSecret(keys.slice(0, i));
+      recoveredSecrets.push(recoveredSecret);
+    }
+
+    return NextResponse.json({ recoveredSecrets : recoveredSecrets[0] }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
